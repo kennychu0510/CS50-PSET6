fc60bb4bc1ecd984f30ec469aa40df151b1d276a
Kenny <95134000+kennychu0510@users.noreply.github.com>
2022-03-01 09:58:56 +0800

    Delete movies directory

--- movies/12.sql
+++
@@ -1,5 +0,0 @@
-SELECT title FROM movies
-JOIN ratings ON movies.id = ratings.movie_id
-WHERE id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Johnny Depp"))
-AND id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Helena Bonham Carter"))
-ORDER BY rating DESC;
--- movies/4.sql
+++
@@ -1,1 +0,0 @@
-SELECT COUNT(title) FROM movies JOIN ratings ON movies.id = ratings.movie_id WHERE ratings.rating = "10";
